cb0dbe1bc75a87bf323b927e9c317fb9605ed608
Normal-case <8282chan94@gmail.com>
2022-02-17 11:24:41 +0900

    test video

--- pages/video.jsx
+++ pages/video.jsx
@@ -1,11 +1,43 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 export default function Video() {
+  const refSelectVideo = useRef(null)
+  const refVideo = useRef(null)
+
+  useEffect(() => {
+    listDevices()
+    startWebcam()
+  }, [])
+
   const listDevices = async () => {
     const devices = await navigator.mediaDevices.enumerateDevices()
     devices.forEach(device => {
       console.log(device)
+      switch (device.kind) {
+        case 'audioinput':
+          break
+        case 'videoinput':
+          refSelectVideo.current.appendChild(device.deviceId)
+      }
     })
   }
-  return <>video page</>
+
+  const getParams = video => {
+    return {
+      video: { deviceId: video ? { exact: video } : undefined },
+    }
+  }
+
+  const startWebcam = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia(
+      getParams(refSelectVideo.current.value)
+    )
+
+    refVideo.current.srcObject = stream
+  }
+  return (
+    <>
+      <video ref={refVideo} autoPlay />
+    </>
+  )
 }
