f01ad0730935c5226f274eb989d22d189730ca5a
Normal-case <8282chan94@gmail.com>
2022-02-17 12:49:41 +0900

    video

--- pages/video.jsx
+++ pages/video.jsx
@@ -12,7 +12,9 @@ export default function Video() {
   const listDevices = async () => {
     const devices = await navigator.mediaDevices.enumerateDevices()
     devices.forEach(device => {
-      console.log(device)
+      if (device.kind === 'videoinput') {
+        console.log(device)
+      }
     })
   }
 
