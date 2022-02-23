397ec1c121caad1af22da2c44bcc65c3f2edd2d8
Normal-case <8282chan94@gmail.com>
2022-02-23 09:19:48 +0900

    check device list

--- pages/video.jsx
+++ pages/video.jsx
@@ -1,45 +1,46 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function Video() {
-  const refSelectVideo = useRef(null)
-  const refVideo = useRef(null)
+  const refSelectVideo = useRef(null);
+  const refVideo = useRef(null);
 
   useEffect(() => {
-    listDevices()
+    listDevices();
     // startWebcam()
-  }, [])
+  }, []);
 
   const listDevices = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices()
-    const cameras = devices.filter(device => device.kind == 'videoinput')
-    cameras.forEach(camera => {
-      const option = document.createElement('option')
-      option.value = camera.deviceId
-      option.text = camera.label
-      refSelectVideo.current.appendChild(option)
-    })
-  }
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    console.log(devices);
+    const cameras = devices.filter((device) => device.kind == 'videoinput');
+    cameras.forEach((camera) => {
+      const option = document.createElement('option');
+      option.value = camera.deviceId;
+      option.text = camera.label;
+      refSelectVideo.current.appendChild(option);
+    });
+  };
 
-  const getParams = video => {
+  const getParams = (video) => {
     return {
       video: { deviceId: video ? { exact: video } : undefined },
-    }
-  }
+    };
+  };
 
   const startWebcam = async () => {
     const stream = await navigator.mediaDevices
       .getUserMedia(getParams(refSelectVideo.current.value))
       .then(
-        stream => (refVideo.current.srcObject = stream),
-        error => console.log(error)
-      )
-  }
+        (stream) => (refVideo.current.srcObject = stream),
+        (error) => console.log(error),
+      );
+  };
   return (
     <>
       <div>
-        <select ref={refSelectVideo} onChange={e => startWebcam()} />
+        <select ref={refSelectVideo} onChange={(e) => startWebcam()} />
       </div>
       <video ref={refVideo} autoPlay />
     </>
-  )
+  );
 }
